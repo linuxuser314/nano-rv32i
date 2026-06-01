d947819e985865572d4edc4edf9f506da0c4b44a
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-01 19:31:01 -0400

    feat(mux2): Add 2-input mux module

---
+++ rtl/mux2.sv
@@ -0,0 +1,6 @@
+module mux2(input  logic[31:0] in0, in1,
+            input  logicj      select,
+            output logic[31:0] result
+        );
+        assign result = select ? in1 : in0;
+endmodule
