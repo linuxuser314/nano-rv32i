edc65f3536ab304e47015359e1136c1b340fc580
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-01 14:07:10 -0400

    docs(byte_shift_left, byte_shift_right): Added LUT usage and propogation delay to documentation

--- rtl/byte_shift_right.sv
+++ rtl/byte_shift_right.sv
@@ -1,5 +1,5 @@
 //This module takes a 4-byte word and shifts it to the right one byte at a time.
-
+//Estimated resouce usage: 64 LUTs (32 LUTs/stage x 2 stages), 2-LUT propogation delay
 module byte_shift_right(input logic[31:0] data,
                         input logic shift1, shift2, fill_bit,
                         output logic[31:0] result
