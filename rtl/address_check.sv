303733f2826cb11bbd135b3c3e881aecf272e311
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-01 14:48:20 -0400

    docs(address_check): Update address_check to use ADDRESS_MISALIGNED_ERROR to make it more generic to be used on instruction fetches  as well as loads/stores.

--- rtl/address_check.sv
+++ rtl/address_check.sv
@@ -4,7 +4,7 @@
 
 module address_check(input  logic[1:0] addr_end,
                      input  logic      is_byte, is_half,
-                     output logic      MEMORY_MISALIGNED_ERROR);
+                     output logic      ADDRESS_MISALIGNED_ERROR);
     //If it's a word and it's not 4 bit aligned or it's a half and it's not 2-bit aligned, enable the error line.
     assign MEMORY_MISALIGNED_ERROR = ~(is_byte | is_half) & (addr_end[0] | addr_end[1]) |
                                       (is_half & addr_end[0]);
