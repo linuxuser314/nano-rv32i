f7d08c0059f8495e478d965f3e7e6bc995e8c9b9
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-01 14:09:04 -0400

    docs(store): Added estimated LUT count and propogation delay

--- rtl/store.sv
+++ rtl/store.sv
@@ -1,7 +1,7 @@
 //This module shifts, masks, and sets the write enble lines for sw/sh/sb commands.
 //It uses one-hot enables for isHalf and isByte.
 //It assumes that memory accesses are properly aligned. Unaligned stores are undefined behavior.
-
+//Unit consumes 68 LUTs (2 from byte_shift_left, 1 from each write_enable set) and has a 2-LUT propogation delay on result and 1-LUT propogation delay on write_enable.
 `default_nettype none
 
 module store(input  logic[31:0] data,
@@ -14,12 +14,8 @@ module store(input  logic[31:0] data,
         .data(data), .shift1(addr_end[0]), .shift2(addr_end[1]), .fill_bit(1'b0), .result(result)
     );
 
-    logic is_word;
-    assign is_word = ~(is_half | is_byte);
-
     //Like in the load fill_bit calculator, this is a lot of combinational logic!
-    //It is difficult to read and I should probably clean it up.
-    //There is also a lot of duplicated logic and I'm not sure if Yosys will optimize it away for me or not, so I should probably optimize it at some point.
+    //It is difficult to read and I should probably clean it up. However, it synthesizes to 4 LUTs with 1-LUT propogation delay so that's great.
     //Basically I'm setting each WE bit if it is a word OR if it's a half that's on that byte OR it's a byte on that byte.
     assign write_enable[0] =  ~(is_half | is_byte) |
                              (is_half & ~addr_end[1]) |
