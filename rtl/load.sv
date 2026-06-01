34b6b82e2b5d683e31850b70417e3dd15fca4fac
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-01 14:14:43 -0400

    docs(load): Add estimated LUT usage and propogation delay

--- rtl/load.sv
+++ rtl/load.sv
@@ -1,5 +1,6 @@
 //This module shifts and extends a memory word for lw, lh, lb, lhu, lbu commands in RISC-V.
-
+//Estimated LUT usage: 70 (2 for shift calculations + 64 for shifter + 4 for fill_bit calculation)
+//Estimated propogation delay: 5-LUTs (3 for fill_bit calculation and 2 for shifting).
 `default_nettype none;
 module load(input  logic[31:0] data,
             input  logic is_byte, is_half, is_unsigned,
@@ -14,7 +15,8 @@ module load(input  logic[31:0] data,
     assign shift1 = is_byte & addr_end[0];
 
     //This chunk of logic selects the fill bit for the shifter. It's slightly complex and should probably be refactored for readability.
-    //It's almost pure combinational logic but that may be obscuring the purpose.
+    //It's pure logic-gate combinational logic but that may be obscuring the purpose.
+    //It's a 9-input boolean function, which I am going to guess maps to 4 LUTs with a 3-LUT delay (conservative guess)
     //If it's unsigned, the sign bit is 0. Otherwise:
     assign fill_bit = is_unsigned ? 0 :
                       //First line pulls out the sign bit of Byte 1 if it's fetching Byte 1, second line does the same for Byte 3.
