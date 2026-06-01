a26878d5e129dbbea84c0f5cc89d0d3e76191010
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-01 14:51:02 -0400

    feat(memory): Added dummy memory module

---
+++ rtl/memory.sv
@@ -0,0 +1,7 @@
+//This is a dummy module for memory since I don't know how to instantiate BRAM
+
+module memory(input  logic[31:0] A1, A2, WD2,
+              output logic[31:0] RD1, RD2,
+              output logic MEMORY_OUT_OF_BOUNDS_ERROR);
+    //Real code would go here...
+endmodule
