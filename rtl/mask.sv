67f54eb7194b795a4ae161c14b0e5e3c02f4878d
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-01 11:17:03 -0400

    fix(mask): Fixed result typo, removed unique, and added default case to handle floating/contention in inputs.

--- rtl/mask.sv
+++ rtl/mask.sv
@@ -1,13 +1,13 @@
 `default_nettype none;
 module mask(input logic[31:0] a, b,
             input logic[1:0] ctrl,
-            output logic[31:0] reult);
+            output logic[31:0] result);
     always_comb begin
-        unique case(ctrl)
+        case(ctrl)
             0: result = a & b;
             1: result = a | b;
             2: result = a ^ b;
-            3: result = 0;
+            default result = 0;
         endcase
     end
 endmodule
