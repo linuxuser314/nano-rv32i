d01f054169a184549e0269c84af124f10692d344
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-05 22:34:25 +0000

    fix(bne): Trying to fix bne
    -It appears that when it hits a sub command bne is breaking (or something like this). I don't know exactly what's happening but I know bne is misbehaving.
    -This is an attempt to fix it. I have not fully succeeded. However, I am making progress.

--- sim/basic_instruction_test.s
+++ sim/basic_instruction_test.s
@@ -7,24 +7,28 @@ _start:
 
     # 3. INSTRUCTIONS (The actual RISC-V code)
 
-    #x1 = 8
+    #x1 = 8 - PASSED
     addi x1, x0, 5
     addi x1, x1, 3
 
-    #x1 = 64
+    #x1 = 64 - PASSED
     slli x1, x1, 3
-    #x1 = 2
+
+    #x1 = 2 - PASSED
     srli x1, x1, 5
+
+    #loads multiple immediates with lui and addi
+    li x2, 1000000000
+    li x3, 1000000002
+    sub x4, x3, x2
+    #x4 = 1000002 - 100000 = 2
+
+    #if they are equal it will continue to li into x5
+    bne x4, x1, end
+    li x5, 16
 
 
     j end
-
-fail:
-    addi x3, x0, 1   # Set error code 1
-    jal x0, end      # Jump to the end
-
-success:
-    addi x3, x0, 999 # Set success code
-
 end:
+    nop              # So I can visualize that it has got to the end because the PC is flip-flopping
     jal x0, end      # Infinite loop to stop the program
