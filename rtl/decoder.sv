639f00263e88782a3330fac37bc2bc7dc50cbcf1
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-05 00:31:30 +0000

    feat(decoder): Add branches, lui, jal, jalr, auipc to decoder

--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -17,6 +17,7 @@ module decoder(input  logic[31:0] instruction,
     always_comb begin
         casez(instruction_data)
         //Format opcode_funct3_funct7
+            //ALU I and R-type instructions
             17'b0110011_000_0100000: begin ALU_src = 1; sub = 1; result_select = 3; end//sub
             17'b0110011_000_0000000: begin ALU_src = 1; result_select = 3; end//add
             17'b0010011_000_???????: begin I = 1; result_select = 3; end//addi
@@ -30,13 +31,26 @@ module decoder(input  logic[31:0] instruction,
             17'b0010011_100_???????: begin I = 1; result_select = 1; sub = 1; mask_ctrl = 2; end//xori
             17'b0110011_101_0000000: begin ALU_src = 1; result_select = 3; is_right_shift = 1; end//srl
             17'b0010011_101_0000000: begin I = 1; result_select = 3; is_right_shift = 1; end//srli 
-            17'b0110011_110_0000000: begin ALU_src = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//sra
-            17'b0010011_110_0000000: begin I = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//srai
+            17'b0110011_101_0100000: begin ALU_src = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//sra
+            17'b0010011_101_0100000: begin I = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//srai
             17'b0110011_110_0000000: begin ALU_src = 1; result_select = 1; mask_ctrl = 2; end//or
             17'b0010011_110_???????: begin I = 1; result_select = 1; sub = 1; mask_ctrl = 2; end//ori
             17'b0110011_111_0000000: begin ALU_src = 1; result_select = 1; mask_ctrl = 2; end//and
             17'b0010011_111_???????: begin I = 1; result_select = 1; sub = 1; mask_ctrl = 2; end//andi
-            //Not done with these...
+
+            //Branches
+            17'b1100011_000_???????: begin B = 1; PC_select = 3; sub = 1; eq = 1; end//beq
+            17'b1100011_001_???????: begin B = 1; PC_select = 3; sub = 1; eq = 1; negate = 1; end//bneq
+            17'b1100011_100_???????: begin B = 1; PC_select = 3; sub = 1; lt = 1; end//blt
+            17'b1100011_101_???????: begin B = 1; PC_select = 3; sub = 1; lt = 1; negate = 1; end//bge
+            17'b1100011_110_???????: begin B = 1; PC_select = 3; sub = 1; ltu = 1; end //bltu
+            17'b1100011_111_???????: begin B = 1; PC_select = 3; sub = 1; ltu = 1; negate = 1; end//bgeu
+
+            //Oddball U and J-type instructions
+            17'b0010111_???_???????: begin U = 1; result_select = 7; end//auipc
+            17'b0110111_???_???????: begin U = 1; result_select = 4; end//lui
+            17'b1100111_000_???????: begin I = 1; result_select = 6; PC_select = 2; end//jalr
+            17'b1101111_???_???????: begin J = 1; result_select = 6; PC_select = 1; end//jal
         endcase
     end
 endmodule
