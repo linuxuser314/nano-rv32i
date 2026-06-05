c911eda08c0de27300633602279a4a60b8aff5d5
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-05 00:12:56 +0000

    fix(decoder): Fixed incomprehensivle case expression error

--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -17,25 +17,25 @@ module decoder(input  logic[31:0] instruction,
     always_comb begin
         casez(instruction_data)
         //Format opcode_funct3_funct7
-            17'b0110011_000_0100000: ALU_src = 1; sub = 1; result_select = 3;//sub
-            17'b0110011_000_0000000: ALU_src = 1; result_select = 3;//add
-            17'b0010011_000_???????: I = 1; result_select = 3;//addi
-            17'b0110011_001_0000000: ALU_src = 1; result_select = 3;//sll
-            17'b0010011_001_0000000: I = 1; result_select = 3;//slli
-            17'b0110011_010_0000000: ALU_src = 1; result_select = 2; sub = 1; lt = 1;//slt
-            17'b0010011_010_???????: I = 1; result_select = 2; sub = 1; lt = 1;//slti
-            17'b0110011_011_0000000: ALU_src = 1; result_select = 2; sub = 1; ltu = 1;//sltu
-            17'b0010011_011_???????: I = 1; result_select = 2; sub = 1; ltu = 1;//sltiu
-            17'b0110011_100_0000000: ALU_src = 1; result_select = 1; mask_ctrl = 2;//xor
-            17'b0010011_100_???????: I = 1; result_select = 1; sub = 1; mask_ctrl = 2;//xori
-            17'b0110011_101_0000000: ALU_src = 1; result_select = 3; is_right_shift = 1;//srl
-            17'b0010011_101_0000000: I = 1; result_select = 3; is_right_shift = 1;//srli
-            17'b0110011_110_0000000: ALU_src = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1;//sra
-            17'b0010011_110_0000000: I = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1;//srai
-            17'b0110011_110_0000000: ALU_src = 1; result_select = 1; mask_ctrl = 2;//or
-            17'b0010011_110_???????: I = 1; result_select = 1; sub = 1; mask_ctrl = 2;//ori
-            17'b0110011_111_0000000: ALU_src = 1; result_select = 1; mask_ctrl = 2;//and
-            17'b0010011_111_???????: I = 1; result_select = 1; sub = 1; mask_ctrl = 2;//andi
+            17'b0110011_000_0100000: begin ALU_src = 1; sub = 1; result_select = 3; end//sub
+            17'b0110011_000_0000000: begin ALU_src = 1; result_select = 3; end//add
+            17'b0010011_000_???????: begin I = 1; result_select = 3; end//addi
+            17'b0110011_001_0000000: begin ALU_src = 1; result_select = 3; end//sll
+            17'b0010011_001_0000000: begin I = 1; result_select = 3; end//slli
+            17'b0110011_010_0000000: begin ALU_src = 1; result_select = 2; sub = 1; lt = 1; end//slt
+            17'b0010011_010_???????: begin I = 1; result_select = 2; sub = 1; lt = 1; end//slti
+            17'b0110011_011_0000000: begin ALU_src = 1; result_select = 2; sub = 1; ltu = 1; end//sltu
+            17'b0010011_011_???????: begin I = 1; result_select = 2; sub = 1; ltu = 1; end//sltiu
+            17'b0110011_100_0000000: begin ALU_src = 1; result_select = 1; mask_ctrl = 2; end//xor
+            17'b0010011_100_???????: begin I = 1; result_select = 1; sub = 1; mask_ctrl = 2; end//xori
+            17'b0110011_101_0000000: begin ALU_src = 1; result_select = 3; is_right_shift = 1; end//srl
+            17'b0010011_101_0000000: begin I = 1; result_select = 3; is_right_shift = 1; end//srli 
+            17'b0110011_110_0000000: begin ALU_src = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//sra
+            17'b0010011_110_0000000: begin I = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//srai
+            17'b0110011_110_0000000: begin ALU_src = 1; result_select = 1; mask_ctrl = 2; end//or
+            17'b0010011_110_???????: begin I = 1; result_select = 1; sub = 1; mask_ctrl = 2; end//ori
+            17'b0110011_111_0000000: begin ALU_src = 1; result_select = 1; mask_ctrl = 2; end//and
+            17'b0010011_111_???????: begin I = 1; result_select = 1; sub = 1; mask_ctrl = 2; end//andi
             //Not done with these...
         endcase
     end
