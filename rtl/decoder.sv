9d9711f5d1fb76c6e52a4d039c5176d4f699a6db
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-05 21:26:38 +0000

    fix(shifter): Put correct result_select values in the decoder to trigger the shifter on shift instructions

--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -35,16 +35,16 @@ module decoder(input  logic[31:0] instruction,
                 17'b0010011_000_???????: begin RF_write_enable = 1; I = 1; result_select = 3; end//addi
                 17'b0110011_001_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 3; end//sll
                 17'b0010011_001_0000000: begin RF_write_enable = 1; I = 1; result_select = 3; end//slli
-                17'b0110011_010_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 2; sub = 1; lt = 1; end//slt
-                17'b0010011_010_???????: begin RF_write_enable = 1; I = 1; result_select = 2; sub = 1; lt = 1; end//slti
+                17'b0110011_010_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 0; sub = 1; lt = 1; end//slt
+                17'b0010011_010_???????: begin RF_write_enable = 1; I = 1; result_select = 0; sub = 1; lt = 1; end//slti
                 17'b0110011_011_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 2; sub = 1; ltu = 1; end//sltu
                 17'b0010011_011_???????: begin RF_write_enable = 1; I = 1; result_select = 2; sub = 1; ltu = 1; end//sltiu
                 17'b0110011_100_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 1; mask_ctrl = 2; end//xor
                 17'b0010011_100_???????: begin RF_write_enable = 1; I = 1; result_select = 1; mask_ctrl = 2; end//xori
-                17'b0110011_101_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 3; is_right_shift = 1; end//srl
-                17'b0010011_101_0000000: begin RF_write_enable = 1; I = 1; result_select = 3; is_right_shift = 1; end//srli
-                17'b0110011_101_0100000: begin RF_write_enable = 1; ALU_src = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//sra
-                17'b0010011_101_0100000: begin RF_write_enable = 1; I = 1; result_select = 3; is_right_shift = 1; is_arithmetic_shift = 1; end//srai
+                17'b0110011_101_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 0; is_right_shift = 1; end//srl
+                17'b0010011_101_0000000: begin RF_write_enable = 1; I = 1; result_select = 0; is_right_shift = 1; end//srli
+                17'b0110011_101_0100000: begin RF_write_enable = 1; ALU_src = 1; result_select = 0; is_right_shift = 1; is_arithmetic_shift = 1; end//sra
+                17'b0010011_101_0100000: begin RF_write_enable = 1; I = 1; result_select = 0; is_right_shift = 1; is_arithmetic_shift = 1; end//srai
                 17'b0110011_110_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 1; mask_ctrl = 1; end//or
                 17'b0010011_110_???????: begin RF_write_enable = 1; I = 1; result_select = 1; mask_ctrl = 1; end//ori
                 17'b0110011_111_0000000: begin RF_write_enable = 1; ALU_src = 1; result_select = 1; mask_ctrl = 0; end//and
