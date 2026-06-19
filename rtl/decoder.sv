d27fa0048541481df33c88b90acd366a631b3474
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-19 20:43:51 +0000

    fix(decoder): Fix decoder to include I=1 for proper immediate decoding of non-word memory operations. Address is still necessary for shifting/masking. Now it passes load/store tests.

--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -101,6 +101,7 @@ module decoder(input  logic[31:0] instruction,
                     is_unsigned = load_sign_bit;//The start of funct3 is a sign flag
                     is_half = load_size_bit_0;//The last bit signals a half
                     is_byte = load_size_bit_0 ~| load_size_bit_1; //If neitheir of the last two bits are 1 then it's a byte load
+                    I = 1;
                 end
             end
         end
