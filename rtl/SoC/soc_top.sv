d026ca90973409bc94e7e393d38161a4d8511422
Creed E. Truman <creed_truman77@my.albemarle.edu>
2026-06-27 17:12:10 +0000

    feat(SoC): Mostly finished top-level soc_top module

--- rtl/SoC/soc_top.sv
+++ rtl/SoC/soc_top.sv
@@ -19,46 +19,10 @@ module soc_top(input  logic      clk_27MHz, reset_button,
     logic sys_clk, sys_reset;
     assign sys_clk = clk_27MHz;
     assign sys_reset = reset_button;
-    /*
-    //Master0 (core0_fetch) internal signals
-    logic[31:0] fetch_addr, fetch_data;
-    logic       fetch_enable;
-    logic       FETCH_FAULT;
 
-    //Master1 (core0_data) internal signals
-    logic[31:0] data_addr, load_data, store_data;
-    logic       data_read_enable, data_write_enable;
-    logic[3:0]  data_write_enable_control;
-    logic DATA_FAULT;
+    logic MMIO_FAULT;
+    logic core0_FETCH_FAULT, core0_DATA_FAULT;
 
-    //Slave0 (boot_rom) internal signals
-    logic        boot_rom_read_enable;
-    logic [31:0] boot_rom_addr, boot_rom_result;
-
-    //Slave1 (payload_rom) internal signals
-    logic        payload_rom_read_enable;
-    logic [31:0] payload_rom_addr, payload_rom_result;
-
-    //Slave2 (mmio_controller) internal signals
-    logic        mmio_controller_read_enable;
-    logic [31:0] mmio_controller_addr, mmio_controller_result;
-    logic        mmio_controller_write_enable;
-    logic [31:0] mmio_controller_write_data;
-
-    //Slave3 (system_ram0_A) internal signals
-    logic        system_ram0_read_enable_A;
-    logic [31:0] system_ram0_addr_A, system_ram0_result_A;
-    logic        system_ram0_write_enable_A;
-    logic [3:0]  system_ram0_write_enable_control_A;
-    logic [31:0] system_ram0_write_data_A;
-
-    //Slave4 (system_ram0_B) internal signals
-    logic        system_ram0_read_enable_B;
-    logic [31:0] system_ram0_addr_B, system_ram0_result_B;
-    logic        system_ram0_write_enable_B;
-    logic [3:0]  system_ram0_write_enable_control_B;
-    logic [31:0] system_ram0_write_data_B;
-    */
     rom_bus_if core0_fetch_bus(); //Master 0
     ram_bus_if core0_data_bus(); //Master 1
 
@@ -66,73 +30,48 @@ module soc_top(input  logic      clk_27MHz, reset_button,
     rom_bus_if payload_rom_bus(); // Slave 1
     ram_bus_if mmio_bus(); //Slave 2
 
-    ram_bus_if system_ram0_A(); // Slave 3
-    ram_bus_if system_ram0_B(); // Slave 4
+    ram_bus_if system_ram0_bus_A(); // Slave 3
+    rom_bus_if system_ram0_bus_B(); // Slave 4 (ROM for fetching)
 
 
     //Master 0/1 (core0_fetch, core0_data)
     rv32i_core core0(
+
         //Instruction fetching
-        .fetch_addr(fetch_addr), .fetch_data(fetch_data),
-        .fetch_enable(fetch_enable),
-        .FETCH_FAULT(FETCH_FAULT),
+        .fetch_bus(core0_fetch_bus),
+        .data_bus(core0_data_bus),
 
         //Data reading/writing
-        .data_addr(data_addr), .load_data(load_data), .store_data(store_data),
-        .data_read_enable(data_read_enable), .data_write_enable(data_write_enable),
-        .data_write_enable_control(data_write_enable_control),
-        .DATA_FAULT(DATA_FAULT),
+        .DATA_FAULT(core0_DATA_FAULT),
+        .FETCH_FAULT(core0_FETCH_FAULT),
 
-        //Simple passthrough (for now)
         .clk(sys_clk), .reset(sys_reset)
     );
 
     //Master-slave bus interconnect
     bus_interconnect master_bus(
         //Master 0 (core0 fetch)
-        /*
-        .fetch_addr(fetch_addr), .fetch_data(fetch_data),
-        .fetch_enable(fetch_enable),*/
         .fetch_bus(core0_fetch_bus),
-        .FETCH_FAULT(FETCH_FAULT),
+        .FETCH_FAULT(core0_FETCH_FAULT),
 
         //Master 1 (core0 data)
-        /*
-        .data_addr(data_addr), .load_data(load_data), .store_data(store_data),
-        .data_read_enable(data_read_enable), .data_write_enable(data_write_enable),
-        .data_write_enable_control(data_write_enable_control),*/
         .data_bus(core0_data_bus),
-        .DATA_FAULT(DATA_FAULT),
+        .DATA_FAULT(core0_DATA_FAULT),
 
         //Slave 0 (boot_rom)
-        .boot_rom_read_enable(boot_rom_read_enable),
-        .boot_rom_addr(boot_rom_addr),
-        .boot_rom_result(boot_rom_result),
+        .boot_rom_bus(boot_rom_bus),
 
         //Slave 1 (payload_rom)
-        .payload_rom_read_enable(payload_rom_read_enable),
-        .payload_rom_addr(payload_rom_addr),
-        .payload_rom_result(payload_rom_result),
+        .payload_rom_bus(payload_rom_bus),
 
         //Slave 2 (mmio_controller)
-        .mmio_controller_read_enable(mmio_controller_read_enable),
-        .mmio_controller_addr(mmio_controller_addr),
-        .mmio_controller_result(mmio_controller_result),
-        .mmio_controller_write_enable(mmio_controller_write_enable),
-        .mmio_controller_write_data(mmio_controller_write_data),
+        .mmio_bus(mmio_bus),
+        .MMIO_FAULT(MMIO_FAULT),
 
         //Slave 3 (system_ram0_A)
-        .system_ram_read_enable_A(system_ram0_read_enable_A),
-        .system_ram_addr_A(system_ram0_addr_A),
-        .system_ram_result_A(system_ram0_result_A),
-        .system_ram_write_enable_A(system_ram0_write_enable_A),
-        .system_ram_write_data_A(system_ram0_write_data_A),
-        .system_ram_write_enable_control_A(system_ram0_write_enable_control_A),
+        .system_ram0_bus_A(system_ram0_bus_A),
+        .system_ram0_bus_B(system_ram0_bus_B)
 
-        //Slave 4(system_ram0_B)
-        .system_ram_read_enable_B(system_ram0_read_enable_B),
-        .system_ram_addr_B(system_ram0_addr_B),
-        .system_ram_result_B(system_ram0_result_B)
     );
 
     //Slave 0 (boot_rom)
@@ -140,8 +79,8 @@ module soc_top(input  logic      clk_27MHz, reset_button,
         .FILE_PATH("/workspaces/nano-rv32i/software/boot_rom.hex"),
         .SIZE(2304)
     ) boot_rom(
-        .clk(sys_clk), .read_enable(boot_rom_read_enable),
-        .address(boot_rom_addr), .result(boot_rom_result)
+        .clk(sys_clk),
+        .bus(boot_rom_bus)
     );
 
     //Slave 1 (payload_rom)
@@ -149,17 +88,16 @@ module soc_top(input  logic      clk_27MHz, reset_button,
         .FILE_PATH("/workspaces/nano-rv32i/software/payload_rom.hex"),
         .SIZE(2304)
     ) payload_rom(
-        .clk(sys_clk), .read_enable(payload_rom_read_enable),
-        .address(payload_rom_addr), .result(payload_rom_result)
+        .clk(sys_clk),
+        .bus(payload_rom_bus)
     );
 
     //Slave 2 (mmio_controller)
     mmio mmio_controller(
-        .clk(sys_clk), .read_enable(mmio_controller_read_enable),
-        .address(mmio_controller_addr), .result(mmio_controller_result),
-        .write_enable(mmio_controller_write_enable),
-        .write_data(mmio_controller_write_data),
-        .led_strip6(led_strip6)
+        .clk(sys_clk),
+        .mmio_bus(mmio_bus),
+        .led_strip6(led_strip6),
+        .MMIO_FAULT(MMIO_FAULT)
     );
 
     //Slave 3/4 (system_ram0)
@@ -167,14 +105,9 @@ module soc_top(input  logic      clk_27MHz, reset_button,
         .clk(sys_clk),
 
         //Slave3 (system_ram0_A)
-        .read_enable_A(system_ram0_read_enable_A),
-        .address0_A(system_ram0_addr_A), .result_A(system_ram0_result_A),
-        .write_enable_A(system_ram0_write_enable_A),
-        .write_enable_control_A(system_ram0_write_enable_control_A),
-        .write_data_A(system_ram0_write_data_A),
+        .bus_A(system_ram0_bus_A),
 
         //Slave4 (system_ram0_B)
-        .read_enable_B(system_ram0_read_enable_B),
-        .address1_B(system_ram0_addr_B), .result_B(system_ram0_result_B)
+        .bus_B(system_ram0_bus_B)
     );
 endmodule
